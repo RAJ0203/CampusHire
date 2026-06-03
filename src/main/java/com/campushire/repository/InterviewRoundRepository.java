a0177d3881306086fbb3472fde296ee1f7341d94
mishra-bhavya <mishrabhavya.bm@gmail.com>
2026-06-03 13:45:40 +0530

    Add interview round status filtering endpoint

--- src/main/java/com/campushire/repository/InterviewRoundRepository.java
+++ src/main/java/com/campushire/repository/InterviewRoundRepository.java
@@ -8,9 +8,12 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+import com.campushire.enums.InterviewStatus;
+
 public interface InterviewRoundRepository extends JpaRepository<InterviewRound, Long> {
 
     List<InterviewRound> findByApplicationId(Long applicationId);
+    List<InterviewRound> findByStatus(InterviewStatus status);
     Page<InterviewRound> findAll(Pageable pageable);
 
 }
